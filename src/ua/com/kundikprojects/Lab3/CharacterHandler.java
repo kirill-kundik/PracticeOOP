a74847b655dc0f5b275ed4799a06b579af61aa82
kirill-kundik <goliathusua@gmail.com>
2018-05-31 06:04:42 +0300

    fixed Lab03

--- src/ua/com/kundikprojects/Lab3/CharacterHandler.java
+++ src/ua/com/kundikprojects/Lab3/CharacterHandler.java
@@ -1,16 +1,10 @@
 package ua.com.kundikprojects.Lab3;
 
 import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 
 public class CharacterHandler {
-    //Java 7 source level
-    public static void main(String[] args) {
-        handleFile("E:\\workspace\\PracticeOOP\\src\\ua\\com\\kundikprojects\\Lab3\\test.txt", "E:\\workspace\\PracticeOOP\\src\\ua\\com\\kundikprojects\\Lab3\\made.txt");
-    }
 
-    private static void handleFile(String inputFilename, String outputFilename) {
+    public static void handleFile(String inputFilename, String outputFilename) {
         try (BufferedReader reader = new BufferedReader(new FileReader(inputFilename));
              BufferedWriter writer = new BufferedWriter(new FileWriter(outputFilename))) {
             int r;
